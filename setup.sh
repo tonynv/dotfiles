be14955e4e9d01ef7dc03a99ca997c07d81ef6f3
Tony Vattathil <avattathil@gmail.com>
2018-03-08 21:22:27 -0800

    Install Xcode and HomeBrew dependancies

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+# Install xcode (requires prompts) 
+xcode-select --install
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 # Clone dotfiles
 git clone --recursive git@github.com:avattathil/dotfiles.git && mv dotfiles .dotfiles  
 # Install tmux configs
